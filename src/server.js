2674a9f8dcd074e6fdffd2b2864ee9e2d70038b3
deykris777 <krishnakumardey452@gmail.com>
2026-05-20 02:33:39 +0530

    diagnostics: add HTTP request logger and verbose browser console logs

--- src/server.js
+++ src/server.js
@@ -13,6 +13,10 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  logger.info(`🌐 ${req.method} ${req.url}`);
+  next();
+});
 app.use(express.static(path.join(__dirname, '../dashboard')));
 
 // Routes
